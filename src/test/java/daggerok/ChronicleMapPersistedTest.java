3599d4be82f40688f82a409ffad38f09940a4717
Maksim Kostromin <daggerok@gmail.com>
2020-03-28 21:35:41 +0200

    Initial commit

---
+++ src/test/java/daggerok/ChronicleMapPersistedTest.java
@@ -0,0 +1,53 @@
+package daggerok;
+
+import io.vavr.control.Try;
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+import net.openhft.chronicle.values.Values;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.function.Function;
+
+interface PostalCodeRange {
+
+  int minCode();
+
+  void minCode(int minCode);
+
+  int maxCode();
+
+  void maxCode(int maxCode);
+}
+
+class ChronicleMapPersistedTest {
+
+  @Test
+  void test() {
+
+    ChronicleMapBuilder<CharSequence, PostalCodeRange> mapBuilder =
+        ChronicleMapBuilder.of(CharSequence.class, PostalCodeRange.class)
+                           .name("city-postal-codes-map")
+                           .averageKey("Amsterdam")
+                           .entries(50_000);
+    System.out.println(mapBuilder);
+
+    File mapFile = Paths.get("target", "map.db").toFile();
+    System.out.println(mapFile);
+
+    ChronicleMap<CharSequence, PostalCodeRange> map =
+        Try.of(() -> mapBuilder.createOrRecoverPersistedTo(mapFile, false))
+           .onFailure(throwable -> System.out.println(throwable.getLocalizedMessage()))
+           .getOrElseThrow((Function<Throwable, RuntimeException>) RuntimeException::new);
+    System.out.println(map);
+
+    PostalCodeRange rangeInstance = Values.newHeapInstance(PostalCodeRange.class);
+    rangeInstance.minCode(1);
+    rangeInstance.maxCode(123);
+    System.out.println(rangeInstance);
+
+    map.put("Amsterdam", rangeInstance);
+    System.out.println(map);
+  }
+}
